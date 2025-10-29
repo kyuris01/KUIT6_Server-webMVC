d28aa6d1209124b9b292652e4f2b976caa79cf09
Chris Ahn <chrisahn1007@gmail.com>
2025-10-29 11:29:04 +0900

    refactor : UpdateUserController 리팩토링

--- src/main/java/jwp/controller/UpdateUserController.java
+++ src/main/java/jwp/controller/UpdateUserController.java
@@ -1,6 +1,7 @@
 package jwp.controller;
 
 import core.db.MemoryUserRepository;
+import jwp.dao.UserDao;
 import jwp.model.User;
 
 import javax.servlet.ServletException;
@@ -10,20 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebServlet("/user/update")
-//public class UpdateUserController extends HttpServlet {
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"), req.getParameter("email"));
-//        MemoryUserRepository.getInstance().changeUserInfo(user);
-//        System.out.println("user 회원정보 수정 완료");
-//        resp.sendRedirect("/user/list");
-//    }
-//}
 public class UpdateUserController implements Controller {
-    public String process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    private final UserDao userDao;
+
+    public UpdateUserController() {
+        this.userDao = new UserDao();
+    }
+
+    public String process(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"), req.getParameter("email"));
-        MemoryUserRepository.getInstance().changeUserInfo(user);
+        userDao.update(user);
         System.out.println("user 회원정보 수정 완료");
         return "redirect:/user/list";
     }
