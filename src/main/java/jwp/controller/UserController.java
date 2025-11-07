5def1bef7a4ad3fa950cac084ca794a06f4aef70
Chris Ahn <chrisahn1007@gmail.com>
2025-11-07 14:43:21 +0900

    refactor : UserController ë리팩토링

--- src/main/java/jwp/controller/UserController.java
+++ src/main/java/jwp/controller/UserController.java
@@ -5,11 +5,9 @@ import jwp.model.User;
 import jwp.util.UserSessionUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -32,5 +30,58 @@ public class UserController {
             return "/user/list";
         }
         return "redirect:/user/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(HttpSession session, @RequestParam String userId, @RequestParam String password) throws Exception {
+        User loginUser = new User(userId, password);
+        User user = userDao.findByUserId(userId);
+
+        if (user != null && user.isSameUser(loginUser)) {
+            session.setAttribute("user", user);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) throws Exception {
+        session.removeAttribute("user");
+        return "redirect:/";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute User user) throws Exception {
+        userDao.update(user);
+        return "redirect:/user/list";
+    }
+
+    @PostMapping("/updateForm")
+    public String updateForm(HttpSession session, @RequestParam String userId) throws Exception {
+        User user = userDao.findByUserId(userId);
+
+        Object value = session.getAttribute("user");
+
+        if (user != null && value != null) {
+            if (user.equals(value)) {            // 수정되는 user와 수정하는 user가 동일한 경우
+                return "/user/updateForm.jsp";
+            }
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/form")
+    public String form() throws Exception {
+        return "/user/form.jsp";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() throws Exception {
+        return "/user/loginForm.jsp";
+    }
+
+    @GetMapping("/loginFailed")
+    public String loginFailed() throws Exception {
+        return "/user/loginFailed.jsp";
     }
 }
